2bc1afc131b2d38e5e8f341a75dbcafa146ea0cf
Kaia-lii <162760512+Kaia-lii@users.noreply.github.com>
2024-12-13 15:17:16 -0500

    Add files via upload

---
+++ R/female_data.R
@@ -0,0 +1,11 @@
+#' Body weight measurements for female mice on different diets
+#'
+#' Includes measurements for female mice bodyweight and indications of treatment.
+#'
+#' @format A tibble with 24 rows and 2 variables:
+#' \describe{
+#'   \item{Diet}{an indicator denoting the diet the mouse was on}
+#'   \item{Bodyweight}{a float indicating the weight of the mouse}
+#' }
+#' @source {Kaggle, by user Evagoras Georgiou. Link: https://www.kaggle.com/datasets/evagorasgeorgiou/miceweights/data}
+"female_data"
